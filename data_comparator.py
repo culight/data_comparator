0ecfc7d7da2297e3a4b232a1968514892b7e3be0
Demerrick Moton <Demerrick.Moton@milliman.com>
2019-10-06 14:27:32 -0400

    add initial main function and gitingore

---
+++ data_comparator.py
@@ -0,0 +1,16 @@
+"""
+### CODE OWNERS: Demerrick Moton
+
+### OBJECTIVE:
+    Main function to house the implementation of data comparator tool
+
+### DEVELOPER NOTES:
+"""
+# pylint: disable=no-member
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
+    sys.exit(RETURN_CODE)
