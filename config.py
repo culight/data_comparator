c497f88dea44480b4b6c1425e8fc468597bfc1a7
Demerrick Moton <Demerrick.Moton@milliman.com>
2020-08-01 00:24:12 -0400

    revamp project directory

---
+++ config.py
@@ -0,0 +1,17 @@
+
+# checking module configuration
+CHECK_CONFIG = {
+    # string checks
+    'white_space': False,
+    'capitalized': False,
+    'empty_text': False,
+    'numeric_data': False,
+    'odd_text_length_diff': False,
+    'special_char': False
+
+    #numeric checks
+
+    #temporal checks
+
+    #boolean checks
+}
