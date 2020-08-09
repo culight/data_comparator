40525ca680b3bd1c2db61390e7dba264dd6bbf4e
Demerrick Moton <Demerrick.Moton@milliman.com>
2020-08-09 13:30:08 -0400

    added dataset detail viewer functionality

--- data_comparator/app.py
+++ data_comparator/app.py
@@ -1,3 +1,5 @@
+import PyQt5
+from components.dataset import Dataset
 import os
 import sys
 import logging
@@ -11,15 +13,19 @@ from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import *
 from PyQt5 import uic
 
-UI_TEMPLATE = "ui/data_comparator.ui"
+MAIN_UI = "ui/data_comparator.ui"
+DETAIL_DLG = "ui/data_detail_dialog.ui"
 ACCEPTED_INPUT_FORMATS = ["sas7bdat", "csv", "parquet", "json"]
+
+DATASET1 = None
+DATASET2 = None
 
 # =============================================================================
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 # =============================================================================
 
 
-class DatasetColumnsListModel(QAbstractListModel):
+class DatasetColumnsList(QListWidget):
     def __init__(self, cols):
         super(DatasetColumnsListModel, self).__init__()
         self.cols = cols or []
@@ -31,16 +37,6 @@ class DatasetColumnsListModel(QAbstractListModel):
 
     def rowCount(self, index):
         return len(self.cols)
-
-
-class LoadingProgressBar(QProgressBar):
-    def __init__(self, progress_bar):
-        super(QProgressBar, self).__init__()
-
-        self.progress_bar = progress_bar
-
-    def modify_progress(self):
-        print("setting progress bar")
 
 
 class SelectFileButton(QPushButton):
@@ -87,7 +83,7 @@ class SelectFileButton(QPushButton):
         self.onDatasetLoaded()
 
     def onDatasetLoaded(self):
-        self.main.load_columns(self.dataset, self.ds_num)
+        self.main.render(self.dataset, self.ds_num)
 
 
 class ColumnSelectButton(QPushButton):
@@ -95,9 +91,53 @@ class ColumnSelectButton(QPushButton):
         super(QPushButton, self).__init__()
 
 
-class DataDetailButton(QPushButton):
-    def __init__(self, button):
+class DataDetailDialog(QDialog):
+    def __init__(self, dataset):
+        super(DataDetailDialog, self).__init__()
+        uic.loadUi(DETAIL_DLG, self)
+
+        entries = dataset.get_summary()
+        entries.pop("columns")
+        entries = self.get_coltypes(dataset, entries)
+        self.detailDialogTable.setRowCount(len(entries))
+        self.detailDialogTable.setColumnCount(2)
+        for index, (detail_name, detail_val) in enumerate(entries.items()):
+            self.detailDialogTable.setItem(index, 0, QTableWidgetItem(str(detail_name)))
+            self.detailDialogTable.setItem(index, 1, QTableWidgetItem(str(detail_val)))
+        self.detailDialogTable.move(0, 0)
+
+    def get_coltypes(self, dataset, entries):
+        col_type_template = {
+            "string_columns": ["object", "str", "o"],
+            "numeric_columns": ["number", "n", "int"],
+            "time_columns": ["time", "datetime", "date", "t"],
+            "boolean_columns": ["bool", "b"],
+        }
+        for col_type, type_names in col_type_template.items():
+            for type_name in type_names:
+                columns = dataset.get_cols_oftype(type_name).values()
+                if len(columns) == 0:
+                    continue
+                ds_names = [col.name for col in columns]
+                if col_type in entries:
+                    entries[col_type].append(ds_names)
+                else:
+                    entries[col_type] = ds_names
+
+        return entries
+
+
+class DatasetDetailsButton(QPushButton):
+    def __init__(self, button, dataset=None):
         super(QPushButton, self).__init__()
+        self.dataset = dataset
+        self.button = button
+        self.button.clicked.connect(self.onClicked)
+
+    def onClicked(self):
+        if self.dataset != None:
+            detail_dlg = DataDetailDialog(self.dataset)
+            detail_dlg.exec_()
 
 
 class OpenConfigButton(QPushButton):
@@ -120,6 +160,29 @@ class ResetButton(QPushButton):
         super(QPushButton, self).__init__()
 
 
+class DataframeTableModel(QAbstractTableModel):
+    def __init__(self, df):
+        QAbstractTableModel.__init__(self)
+        self.df = df
+
+    def rowCount(self, parent=None):
+        return self.df.shape[0]
+
+    def columnCount(self, parent=None):
+        return self.df.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self.df.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.df.columns[col]
+        return None
+
+
 class LogStream(logging.StreamHandler):
     def __init__(self, parent=None):
         super().__init__()
@@ -140,7 +203,9 @@ class LogStream(logging.StreamHandler):
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        uic.loadUi(UI_TEMPLATE, self)
+        uic.loadUi(MAIN_UI, self)
+
+        self.setup_logger()
 
         self.dataset1_select_file_button = SelectFileButton(
             self, self.dataset1FileLoad, 1
@@ -149,11 +214,9 @@ class MainWindow(QMainWindow):
             self, self.dataset2FileLoad, 2
         )
 
-        self.setupLogger()
-
         self.show()
 
-    def setupLogger(self):
+    def setup_logger(self):
         font = QFont("Arial", 5)
         self.loggingBox.setFont(font)
 
@@ -161,8 +224,25 @@ class MainWindow(QMainWindow):
         logHandler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
         logging.getLogger().addHandler(logHandler)
 
-    def load_columns(self, dataset, ds_num):
-        pass
+    def render(self, dataset, ds_num):
+        if ds_num == 1:
+            DATASET1 = dataset
+
+            # set dataframe table
+            df_model = DataframeTableModel(DATASET1.dataframe)
+            self.dataframe1Table.setModel(df_model)
+            self.ds_details_button1 = DatasetDetailsButton(
+                self.datasetDetails1Button, dataset
+            )
+        if ds_num == 2:
+            DATASET2 = dataset
+
+            # set dataframe table
+            df_model = DataframeTableModel(DATASET2.dataframe)
+            self.dataframe2Table.setModel(df_model)
+            self.ds_details_button2 = DatasetDetailsButton(
+                self.datasetDetails2Button, dataset
+            )
 
 
 if __name__ == "__main__":
