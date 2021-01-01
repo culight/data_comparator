8b2cd0746d4eed52ea9e9b8d040e982db38c846d
culight <dmoton3.14@gmail.com>
2021-01-01 13:37:11 -0500

    add package data

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ import setuptools
 
 setuptools.setup(
     name="data-comparator-dmoton3.14",
-    version='0.5.7',
-    author='Demerrick Moton',
+    version="0.5.9",
+    author="Demerrick Moton",
     author_email="dmoton3.14@gmail.com",
-    packages={
-        "data_comparator",
-        "data_comparator.components",
-        "data_comparator.ui",
-    },
+    packages={"data_comparator", "data_comparator.components", "data_comparator.ui",},
     include_package_data=True,
-    long_description=open('README.md').read(),
+    package_data={
+        "data_comparator.components": ["validations_config.json"],
+        "data_comparator.ui": [
+            "*.ui",
+        ],
+    },
+    description="Data profiling tool with a focus on dataset comparisons",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,5 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     url="https://github.com/culight/data_comparator",
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
